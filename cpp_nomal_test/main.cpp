8ffe15e97097177fb716bcbcd20473946a907c48
Lukas Elmer <lukas.elmer@gmail.com>
2010-03-02 07:39:46 +0000

    cpp

---
+++ cpp_nomal_test/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: Lukas Elmer
+ *
+ * Created on 24. Februar 2010, 12:37
+ */
+
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ cpp_nomal_test/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=cpp_nomal_test
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/cpp_nomal_test
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=cppnomaltest.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/cppnomaltest.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=cpp_nomal_test
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/cpp_nomal_test
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=cppnomaltest.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/cppnomaltest.tar
